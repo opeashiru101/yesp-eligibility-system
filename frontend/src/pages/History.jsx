0e7be977171a0c193b363b9580c0dad4f4c9f5a0
Opemipo Ashiru <opemipo.ashiru@pau.edu.ng>
2026-05-13 17:58:31 +0100

    update API URL

--- frontend/src/pages/History.jsx
+++ frontend/src/pages/History.jsx
@@ -14,7 +14,7 @@ export default function History({ onBack }) {
   const [error, setError] = useState("")
 
   useEffect(() => {
-    axios.get("http://localhost:8000/applications")
+    axios.get("https://yesp-backend-production.up.railway.app")
       .then(res => {
         setApplications(res.data)
         setLoading(false)
